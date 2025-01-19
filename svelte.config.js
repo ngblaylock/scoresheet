d7d29971d59cb878ba122f5d606bdc113558713c
Nathan Blaylock <ngblaylock@gmail.com>
2025-01-19 13:54:56 -0700

    Configure build settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterStatic from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: [sveltePreprocess({
-		scss: {},
-	}), vitePreprocess()],
-
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  preprocess: [
+    sveltePreprocess({
+      scss: {}
+    }),
+    vitePreprocess()
+  ],
+  kit: {
+    adapter: adapterStatic()
+  }
 };
 
 export default config;
