ea660360f97d70426d57aa9e23f55d018fdb6d2f
Nathan Blaylock <ngblaylock@gmail.com>
2025-01-20 10:23:41 -0700

    Routes Added and background grid

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,35 @@
 </script>
 
 <Favicons />
-<Navbar />
 
+
+<div class="grid-bg"></div>
+<Navbar />
 <slot />
+
+{#each Array(100) as x}
+  <div>Lorem {x}</div>
+{/each}
+
+<style lang="scss">
+  .grid-bg {
+    background-color: white;
+    background-image: url('/img/grid.svg');
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: -100;
+    &:after {
+      content: '';
+      background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%);
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    }
+  }
+</style>
---
+++ src/routes/game/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  
+</script>
+
+<h1>Game Scores</h1>
