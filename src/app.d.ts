842bbcc8269289b9ebf2aca3d320a0002f927ba1
Nathan Blaylock <ngblaylock@gmail.com>
2025-02-26 22:46:39 -0700

    Create game object

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,24 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
+  namespace G { // Global Types. Use like G.Player or G.Game
+    interface Player {
+      name: string;
+      rounds: number[];
+    }
+    interface Game {
+      id: number;
+      players: Player[];
+      winnerDeterminate: string;
+    }
+  }
 }
 
 export {};
--- src/routes/setup/+page.svelte
+++ src/routes/setup/+page.svelte
@@ -1,40 +1,95 @@
 <script lang="ts">
   import MainContent from '$components/MainContent.svelte';
+  import { tick } from 'svelte';
+
+  let players = $state([{ name: '' }]);
+  let winnerDeterminate = $state('Highest Score Wins');
+  let winnerOptions = [
+    {
+      label: 'Highest Score Wins',
+      value: 'Highest Score Wins'
+    },
+    {
+      label: 'Lowest Score Wins',
+      value: 'Lowest Score Wins'
+    }
+  ];
+
+  async function addPlayer() {
+    players.push({ name: '' });
+    await tick;
+    const x = document.getElementById(`new-player-${players.length - 1}`);
+    x?.focus();
+  }
+
+  function createGame() {
+    const game: G.Game = {
+      id: Date.now(),
+      players: players
+        .filter((p) => p.name)
+        .map((p) => ({
+          ...p,
+          rounds: []
+        })),
+      winnerDeterminate
+    };
+    window.localStorage.setItem('currentGame', JSON.stringify(game));
+  }
 </script>
 
 <MainContent>
   <div class="container">
     <h1 class="text-center font-cursive">Add Players</h1>
-    <GDevNote>Add Setup Here</GDevNote>
-    <div class="text-center pt-2">
-      <GBtn class="w-100" variant="base-4" iconLeft="plus">Add Player</GBtn>
+    <form autocomplete="off">
+      <GSortableCards bind:items={players}>
+        {#snippet children(player, index)}
+          <div class="hstack align-items-end py-1 px-2 flex-fill">
+            <GTextInput
+              class="flex-fill"
+              label="Player"
+              bind:value={player.name}
+              hideLabel
+              id="new-player-{index}"
+            />
+            <GIconBtn
+              icon="close"
+              variant="base-i4"
+              title="Remove"
+              type="button"
+              onclick={() => {
+                players.splice(index, 1);
+              }}
+            />
+          </div>
+        {/snippet}
+      </GSortableCards>
+    </form>
+    <div class="text-center">
+      <GBtn
+        class="mt-3"
+        variant="base-1"
+        iconLeft="plus"
+        onclick={addPlayer}
+      >
+        Add Player
+      </GBtn>
     </div>
   </div>
   {#snippet actions()}
     <div class="container d-flex flex-column align-items-center gap-3">
       <div>
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            type="radio"
-            name="flexRadioDefault"
-            id="flexRadioDefault1"
-            checked
-          />
-          <label class="form-check-label" for="flexRadioDefault1">Highest Score Wins</label>
-        </div>
-        <div class="form-check">
-          <input
-            class="form-check-input"
-            type="radio"
-            name="flexRadioDefault"
-            id="flexRadioDefault2"
-          />
-          <label class="form-check-label" for="flexRadioDefault2">Lowest Score Wins</label>
-        </div>
+        <GRadioGroup
+          items={winnerOptions}
+          bind:group={winnerDeterminate}
+        />
       </div>
       <div class="text-center">
-        <GBtn href="/game/">Start Game</GBtn>
+        <GBtn
+          disabled={!players.filter((p) => p.name).length}
+          onclick={createGame}
+        >
+          Start Game
+        </GBtn>
       </div>
     </div>
   {/snippet}
