6db49acbe2d12bb71b3e7d72294f1a620115b8b6
Nathan Blaylock <ngblaylock@gmail.com>
2025-03-20 22:18:05 -0600

    Add og data

---
+++ src/routes/SeoMeta.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+  <!-- HTML Meta Tags -->
+  <title>Scoresheet</title>
+  <meta name="description" content="A simple, flexible scorekeeper app for game night fun! Track scores, players, and rounds for card games like Phase 10, Skip-Bo, and Skyjo.">
+  
+  <!-- Facebook Meta Tags -->
+  <meta property="og:url" content="https://scoresheet.nathanblaylock.com/">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Scoresheet">
+  <meta property="og:description" content="A simple, flexible scorekeeper app for game night fun! Track scores, players, and rounds for card games like Phase 10, Skip-Bo, and Skyjo.">
+  <meta property="og:image" content="https://scoresheet.nathanblaylock.com/og-img.jpg">
+  
+  <!-- Twitter Meta Tags -->
+  <meta name="twitter:card" content="summary_large_image">
+  <meta property="twitter:domain" content="scoresheet.nathanblaylock.com">
+  <meta property="twitter:url" content="https://scoresheet.nathanblaylock.com/">
+  <meta name="twitter:title" content="Scoresheet">
+  <meta name="twitter:description" content="A simple, flexible scorekeeper app for game night fun! Track scores, players, and rounds for card games like Phase 10, Skip-Bo, and Skyjo.">
+  <meta name="twitter:image" content="https://scoresheet.nathanblaylock.com/og-img.jpg">
+
+  <!-- Meta Tags Generated via https://www.opengraph.xyz -->
+</svelte:head>
