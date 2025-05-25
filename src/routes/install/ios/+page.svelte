6102892ae3ed6b34d94713f3c5bd38ebba07543e
Nathan Blaylock <ngblaylock@gmail.com>
2025-05-25 00:03:29 -0600

    Add install Instructions, improve device detection

---
+++ src/routes/install/ios/+page.svelte
@@ -0,0 +1,52 @@
+<div class="container my-6 font-cursive text-center">
+  <h1>Install iOS App</h1>
+  <p class="mb-6 lead">
+    You can install this app on your iPhone or iPad as a Progressive Web App (PWA). Just follow these steps using <b>Safari</b>:
+  </p>
+  <div>
+    <p class="mb-6">
+      Step 1: While on this website in Safari, tap the <b>Share</b> button (the square icon with an arrow pointing up).
+      <img
+        src="/img/install/ios-step-1.png"
+        alt="iOS Share Button"
+        class="img-fluid d-block mx-auto mt-3"
+      />
+    </p>
+
+    <p class="mb-6">
+      Step 2: In the share menu, scroll down and select <b>Add to Home Screen</b>.
+      <img
+        src="/img/install/ios-step-2.png"
+        alt="iOS Share Button"
+        class="img-fluid d-block mx-auto mt-3"
+      />
+    </p>
+
+    <p class="mb-6">
+      Step 3: Tap <b>Add</b> in the top right when prompted.
+      <img
+        src="/img/install/ios-step-3.png"
+        alt="iOS Share Button"
+        class="img-fluid d-block mx-auto mt-3"
+      />
+    </p>
+
+    <p class="mb-6">
+      Step 4: A new icon will appear on your Home Screen. Tap it to open the app.
+      <img
+        src="/img/install/ios-step-4.png"
+        alt="iOS Share Button"
+        class="img-fluid d-block mx-auto mt-3"
+      />
+    </p>
+
+    <p class="mb-6">
+      Step 5: The app will now run in full-screen mode, without Safari’s browser interface—just like a native app.
+      <img
+        src="/img/install/ios-step-5.png"
+        alt="iOS Share Button"
+        class="img-fluid d-block mx-auto mt-3"
+      />
+    </p>
+  </div>
+</div>
